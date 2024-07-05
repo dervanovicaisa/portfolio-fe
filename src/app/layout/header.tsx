cde1b6b176c9dd1b8d369c00d1a991deeb45a8d4
Aiša Dervanović <dervanovicaisa@gmail.com>
2024-07-05 16:38:50 +0200

    header and hero section finished

---
+++ src/app/layout/header.tsx
@@ -0,0 +1,43 @@
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary p-4">
+      <div className="container-fluid">
+        <a className="navbar-brand roboto-bold fs-4 font-cursive" href="#">
+          Aisa Dervanovic
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarText"
+        >
+          <div className="form-check form-switch">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label text-muted"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable Chaos
+            </label>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
+import Header from "./layout/header";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Header />
+      <div
+        className={`${styles.portfolioImage}
+          d-flex flex-column align-items-end justify-content-center px-4`}
+      >
+        <div className="w-50">
+          <p className={`${styles.intro} roboto-bold`}>
+            I'am software engineer who builds immersive and user-friendly
+            applications that users love.
+          </p>
+          <p className="roboto-light fs-4">
+            Check out what{" "}
+            <a
+              href="https://ehereal-echoes.blogspot.com/"
+              className="text-dark"
+            >
+              books
+            </a>{" "}
+            I'm reading, find me on{" "}
+            <a
+              href="https://www.linkedin.com/in/aisadervanovic/"
+              className="text-dark"
+            >
+              LinkedIn
+            </a>
+            , or just send me{" "}
+            <a href="mailto:dervanovicaisa@gmail.com" className="text-dark">
+              an email
+            </a>{" "}
+            saying hi.
+          </p>
+          <div className={styles.colors}>
+            <i className="bi bi-circle-fill fs-1 text-dark"></i>
+            <i className="bi bi-circle-fill fs-1 mx-3 text-muted"></i>
+            <i className="bi bi-circle-fill fs-1 text-secondary"></i>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
